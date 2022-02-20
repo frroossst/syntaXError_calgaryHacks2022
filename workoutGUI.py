ad8926b37700249c969387f44116fb55615a54b3
home <adhyanpatel@gmail.com>
2022-02-19 20:58:39 -0600

    GUI made to be more like mobile

--- workoutGUI.py
+++ workoutGUI.py
@@ -1,9 +1,7 @@
-from multiprocessing.sharedctypes import Value
-
-
 class workoutGUI():
 
     import PySimpleGUI as pg
+    import json
     pg.theme("DarkAmber")
 
     def __init__(self) -> None:
@@ -13,9 +11,10 @@ class workoutGUI():
 
         layout=[
             [
-                self.pg.Text('Choose workout type',size=(30, 1), font='Lucida',justification='left',visible=True),
-                self.pg.Listbox(values=["Cardio","Strength"], select_mode='extended', key='fac', size=(30, 6),visible=True),
-                self.pg.Button('Next', font=('Times New Roman',12),visible=True)
+                [(self.pg.Text('Choose workout type',size=(10, 1), font='Lucida',justification='left',visible=True)),
+                self.pg.Listbox(values=["Cardio","Strength"], select_mode='extended', key='fac', size=(20, 4),visible=True)],
+                [self.pg.Button('Select', font=('Times New Roman',12),visible=True),
+                self.pg.Button("Save",font=('Times New Roman',12),visible=True)] 
             ],
             [
                 self.pg.Text("Cardio Workout Details",key="cardio0",visible=False),
@@ -26,14 +25,17 @@ class workoutGUI():
                 self.pg.InputText(1,key="cardio4",visible=False)
             ],
             [
-               self.pg.Text("Strength Worout Details",key="strength0",visible=False),
+               self.pg.Text("Strength Workout Details",key="strength0",visible=False),
                self.pg.InputText("Enter reps",key="strength1",visible=False),
                self.pg.InputText("Enter sets",key="strength2",visible=False),
                self.pg.InputText("Weight",key="strength3",visible=False) 
+            ],
+            [
+
             ]
         ]
 
-        window = self.pg.Window("Window",layout)
+        window = self.pg.Window("Window",layout,size=(400,600))
         
         run = True
         
@@ -42,7 +44,51 @@ class workoutGUI():
             if event == "Cancel" or event == self.pg.WIN_CLOSED:
                 run = False
                 break
-            elif event == "Next" and values["fac"][0] == "Cardio":
+
+            elif event == "Save":
+                workout_record = {}
+            
+                if values["fac"][0] == "Cardio":
+                    workout_record["workoutType"] = "cardio"
+                    if values["cardio1"]:
+                        workout_record["cardioType"] = "Walk"
+                    elif values["cardio2"]:
+                        workout_record["cardioType"] = "Jog"
+                    elif values["cardio3"]:
+                        workout_record["cardioType"] = "Run"
+                    else:
+                        self.pg.popup_error("Press select and/or enter appropriate values")
+                    # Checking if cardio4 is a valid input
+                    if values["cardio4"].isdigit():
+                        if int(values["cardio4"]) >= 0:
+                            workout_record["intended_duration"] = values["cardio4"]
+
+
+                elif values["fac"][0] == "Strength":
+                    workout_record["workoutType"] = "strength"
+
+                    if values["strength1"].isdigit():
+                        if int(values["strength1"]) >= 0:
+                            workout_record["reps"] = values["strength1"]
+                    else:
+                        self.pg.popup_error("Press select and/or enter appropriate values")
+                    if values["strength2"].isdigit():
+                        if int(values["strength2"]) >= 0:
+                             workout_record["sets"] = values["strength2"]
+                    else:
+                        self.pg.popup_error("Press select and/or enter appropriate values")
+                    if values["strength3"].isdigit():
+                        if int(values["strength3"]) >= 0:
+                            workout_record["weight"] = values["strength3"]
+                    else:
+                        self.pg.popup_error("Press select and/or enter appropriate values")
+                
+                print(workout_record)
+                with open("workout_tracker.json","w") as fobj:
+                    self.json.dump(workout_record,fobj,indent=6)
+                    fobj.close()
+            
+            elif event == "Select" and values["fac"][0] == "Cardio":
                 window["cardio1"].Update(visible=True) 
                 window["cardio2"].Update(visible=True)
                 window["cardio3"].Update(visible=True)
@@ -54,7 +100,7 @@ class workoutGUI():
                 window["strength2"].Update(visible=False)
                 window["strength3"].Update(visible=False)
 
-            elif event == "Next" and values["fac"][0] == "Strength":
+            elif event == "Select" and values["fac"][0] == "Strength":
                 window["cardio1"].Update(visible=False) 
                 window["cardio2"].Update(visible=False)
                 window["cardio3"].Update(visible=False)
@@ -65,8 +111,6 @@ class workoutGUI():
                 window["strength1"].Update(visible=True)
                 window["strength2"].Update(visible=True)
                 window["strength3"].Update(visible=True)
-
-            print(values)
 
         window.close()
 
