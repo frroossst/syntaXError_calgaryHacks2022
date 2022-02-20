4199bc680da19c100bae7a7112a67a3618dd880c
Isha Noor <ishanoor@Hifsas-MBP-2.hitronhub.home>
2022-02-19 20:01:43 -0600

    your message saying what you worked on --yourName

---
+++ User.py
@@ -0,0 +1,12 @@
+class User: 
+    def __init__(self, name, WOF, minTime): 
+        self.name = name 
+        self.WOF = WOF 
+        self.minTime = minTime 
+    def get_name(self): 
+        return self.name
+    def get_WOF(self): 
+        return self.WOF
+    def get_minTime(self): 
+        return self.minTime 
+
