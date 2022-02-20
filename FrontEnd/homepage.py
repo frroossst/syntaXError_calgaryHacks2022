a29105f5b0fb5e0e2c99bff090522030ff7d72c1
xGhostEYE <melvin.lugo@hotmail.com>
2022-02-20 01:06:46 -0600

    fixed stuff

--- FrontEnd/homepage.py
+++ FrontEnd/homepage.py
@@ -3,11 +3,11 @@ import PySimpleGUI as sg
 layout = [[sg.Text('Workout Motivator!')],                   
                  [sg.Button("Start Workout")],
                  [sg.Button("Settings")],
-                 [sg.CalendarButton('cal')]
+                 [sg.CalendarButton('calendar')]
                                  
                  ]      
 
-window = sg.Window('App', layout, size=(800,600))    
+window = sg.Window('App', layout, size=(800,600), element_justification='center')    
 while True:
     event, values = window.read()
     print(event,values)
