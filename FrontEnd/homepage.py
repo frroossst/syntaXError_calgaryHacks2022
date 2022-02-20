95ee4382aa42abde0d7d2711074d066330e97a4c
xGhostEYE <melvin.lugo@hotmail.com>
2022-02-20 00:55:14 -0600

    button and calendar

--- FrontEnd/homepage.py
+++ FrontEnd/homepage.py
@@ -1,18 +1,20 @@
-# hello_psg.py
+import PySimpleGUI as sg      
 
-import PySimpleGUI as sg
+layout = [[sg.Text('Workout Motivator!')],                   
+                 [sg.Button("Start Workout")],
+                 [sg.Button("Settings")],
+                 [sg.CalendarButton('cal')]
+                                 
+                 ]      
 
-layout = [[sg.Text("Hello from PySimpleGUI")], [sg.Button("OK")]]
-
-# Create the window
-window = sg.Window("Demo", layout)
-
-# Create an event loop
+window = sg.Window('App', layout, size=(800,600))    
 while True:
     event, values = window.read()
-    # End program if user closes window or
-    # presses the OK button
-    if event == "OK" or event == sg.WIN_CLOSED:
+    print(event,values)
+    
+    if event == sg.WINDOW_CLOSED:
         break
 
-window.close()
+        
+event, values = window.read()    
+window.close()  
